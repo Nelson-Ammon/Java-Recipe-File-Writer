c955b619269c9a688ace785e46d9eaea5e91fc2b
Ammon Nelson <Ammon1nelson@gmail.com>
2021-09-27 18:45:26 -0700

    Project updates

--- Recipe_Input.java
+++ Recipe_Input.java
@@ -8,8 +8,9 @@ public class Recipe_Input {
 
     public static void main(String[] args) throws IOException {
         String text = "";
-        Scanner scanner = new Scanner(System.in);
-        File Recipe_List = new File("Recipe_List.pdf");
+        String amount = "";
+        Scanner scan = new Scanner(System.in);
+        File Recipe_List = new File("Recipe_List.md");
         if(!Recipe_List.exists()){
             System.out.println("File name: 'Recipe_list' Did not exist.");
             System.out.println("We had to make a new file.");
@@ -18,32 +19,53 @@ public class Recipe_Input {
         PrintWriter pw = new PrintWriter(new FileWriter("Recipe_List.md", true));
 
         while (!text.equals("n")) {
+            System.out.println("New Recipe Started");
             System.out.println("Please enter your recipe name:");
-            text = scanner.nextLine();// Take next input from user and save it in "text"
-            pw.write("# " + text + "\n");
-            
+            text = scan.nextLine();// Take next input from user and save it in "text"
+            pw.write("# " + text + "\n\n");
             System.out.println("How many ingredients does your recipe have?");
             int num = 0;
-            int n = scanner.nextInt();
-            while (num <= (n)) {   
-                System.out.println("Ingredient:");
-                text = scanner.nextLine();
-                pw.write("* " + text + "\n");
-                num++;
+            int n = scan.nextInt();  //Takes input of number from user and makes that many ingredient inputs
+            scan.nextLine();// This just eats the \n input from the user
+            pw.write("|Amount/Ingredient|\n");
+            pw.write("|:----------------:|\n");
+            while (num != n ) {   
+                System.out.println("Ingredient: ");
+                text = scan.nextLine();
+                System.out.print("How much " + text + " is needed in the recipe? ");
+                    amount = scan.nextLine();
+                    pw.write("| " + amount + " " + text + "|"+ "\n");
+                    num++;
+                // if(text == "milk"){
+                //     System.out.print("How much " + text + " is needed in the recipe? ");
+                //     amount = scan.nextLine();
+                //     pw.write("* " + amount + " " + text + ""+ "\n");
+                //     num++;
+                // } else{
+                // System.out.print("How many " + text + " is needed in the recipe? ");
+                // amount = scan.nextLine();
+                // pw.write("* " + amount + " " + text + ""+ "\n");
+                // num++;
+                // }
             }
-            // add in a way to make the user input how many ingredients there are then have the computer put that many.
-
-            
+            int count = 1;
+            while (!text.equalsIgnoreCase("done")){
             System.out.println("Please enter the steps to make the recipe:");
-            text = scanner.nextLine();// Take next input from user and save it in "text"
-            pw.write("1. " + text + "\n"); 
-            //Add in a way to have more steps
+            System.out.println("When finished type 'done'");
+            System.out.print(count+". "); 
+            text = scan.nextLine();// Take next input from user and save it in "text"
+            pw.write("__Step "+ count + ".__ " + text + "\n\n");       
+            count++; 
+            }
 
             System.out.println("Add another Recipe? (y/n)");
-            // FOR SOME REASON WHEN YOU TYPE Y IT WILL SKIP THE NEXT PROMPT!!!!!!!
-            text = scanner.next();// Take next input from user and save it in "text"
+
+            text = scan.next();// Take next input from user and save it in "text"
+            scan.nextLine();// Eats the enter \n
         }
+        pw.write("---\n");
+        pw.flush();
         pw.close();
-        System.out.println("completed");
+        System.out.println("Recipe has Succesfully been added!"); // completed program
         }
     }
---
+++ Recipe_List.md
@@ -0,0 +1,37 @@
+# Seasoned Chicken Breast
+|Amount/Ingredient|
+|:----------------:|
+| 2 Breasts Chicken|
+| 2 Tablespoons Brown sugar|
+| 1 Tablespoon Papirka|
+| 1-1/2 teaspoons Oregeno|
+| 1 Teaspoon each Salt & pepper|
+| 2 Tablespoons Olive or Vegtable oil|
+__Step 1.__ Pre-heat oven to 425
+__Step 2.__ thaw the chicken
+__Step 3.__ Combine spices 
+__Step 4.__ rub olive or vegtable oil onto chicken
+__Step 5.__ Spread spices onto chicken
+__Step 6.__ Put the chicken onto a tray that is lined with foil 
+__Step 7.__ put tray into oven and cook for 18 mins
+__Step 8.__ done
+
+---
+# German Pancakes
+
+|Amount/Ingredient|
+|:----------------:|
+| 6 Whipped Eggs|
+| 1 cup milk|
+| 1 cup flour|
+| 1/2 tsp salt|
+| 1/2 tsp vanilla|
+| 1 tbls sugar|
+|1/4-1/2 cup butter|
+__Step 1.__ Whip the eggs and milk together
+__Step 2.__ add the vanilla, flour, salt, and sugar into the whipped eggs and milk
+__Step 3.__ melt 1/4 - 1/2 cup of butter into a 9X13 pan
+__Step 4.__ cook @ 350 for 20-30 mins
+__Step 5.__ done
+
+---
