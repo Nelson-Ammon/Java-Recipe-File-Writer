946a1a4aa403ae49ab3f66088a4f9fabb9914add
Ammon Nelson <Ammon1nelson@gmail.com>
2021-09-28 21:30:01 -0700

    More updates to java file

--- Recipe_Input.java
+++ Recipe_Input.java
@@ -3,69 +3,91 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+//IMPORTS
 
 public class Recipe_Input {
 
     public static void main(String[] args) throws IOException {
         String text = "";
-        String amount = "";
-        Scanner scan = new Scanner(System.in);
-        File Recipe_List = new File("Recipe_List.md");
-        if(!Recipe_List.exists()){
-            System.out.println("File name: 'Recipe_list' Did not exist.");
-            System.out.println("We had to make a new file.");
-            Recipe_List.createNewFile();
-        }
-        PrintWriter pw = new PrintWriter(new FileWriter("Recipe_List.md", true));
+        // SETTING VARS
 
-        while (!text.equals("n")) {
-            System.out.println("New Recipe Started");
-            System.out.println("Please enter your recipe name:");
-            text = scan.nextLine();// Take next input from user and save it in "text"
-            pw.write("# " + text + "\n\n");
-            System.out.println("How many ingredients does your recipe have?");
-            int num = 0;
-            int n = scan.nextInt();  //Takes input of number from user and makes that many ingredient inputs
-            scan.nextLine();// This just eats the \n input from the user
-            pw.write("|Amount/Ingredient|\n");
-            pw.write("|:----------------:|\n");
-            while (num != n ) {   
-                System.out.println("Ingredient: ");
-                text = scan.nextLine();
-                System.out.print("How much " + text + " is needed in the recipe? ");
-                    amount = scan.nextLine();
-                    pw.write("| " + amount + " " + text + "|"+ "\n");
-                    num++;
-                // if(text == "milk"){
-                //     System.out.print("How much " + text + " is needed in the recipe? ");
-                //     amount = scan.nextLine();
-                //     pw.write("* " + amount + " " + text + ""+ "\n");
-                //     num++;
-                // } else{
-                // System.out.print("How many " + text + " is needed in the recipe? ");
-                // amount = scan.nextLine();
-                // pw.write("* " + amount + " " + text + ""+ "\n");
-                // num++;
-                // }
-            }
+        while (!text.equals("n")) { //Checking for the letter n
+           
+            String amount = "";
+            Scanner scan = new Scanner(System.in);
             int count = 1;
-            while (!text.equalsIgnoreCase("done")){
-            System.out.println("Please enter the steps to make the recipe:");
-            System.out.println("When finished type 'done'");
-            System.out.print(count+". "); 
-            text = scan.nextLine();// Take next input from user and save it in "text"
-            pw.write("* __Step "+ count + ".__ " + text + "\n\n");       
-            count++; 
+            File Recipe_List = new File("Recipe_List.md");
+            // CREATED NEW FILE
+            if(!Recipe_List.exists()){
+                System.out.println("File name: 'Recipe_list' Did not exist.");
+                System.out.println("We had to make a new file.");
+                Recipe_List.createNewFile(); 
+                // CHECKS IF FILE ALREAD EXISTS
             }
+            PrintWriter pw = new PrintWriter(new FileWriter("Recipe_List.md", true)); 
+            // if file already exists is just writes on (adds) to that same file
 
-            System.out.println("Add another Recipe? (y/n)");
+
+
+            System.out.println("New Recipe Started");// print to the user
+            System.out.println("Please enter your recipe name:"); //print to the user
+            text = scan.nextLine();// Take next input from user and save it in the var "text"
+            pw.write("# " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase()  + "\n\n"); // Write to the markdown file
+            System.out.println("How many ingredients does your recipe have?");// print to the user
+            
+            int num = 0; // set the var "num" to 0
+            int n = scan.nextInt();  //Takes input of number from user and makes that many ingredient inputs
+            
+            scan.nextLine();// This just eats the \n input from the user
+            
+            pw.write("|Amount/Ingredient|\n");//write to the file
+            pw.write("|:----------------:|\n");
+
+            while (num != n ) {  //while number does not = n do the following 
+                System.out.println("Ingredient: ");// print to user
+                text = scan.nextLine();// get user input
+                int L = text.length();//char var named L
+                char last = text.charAt(L-1);//Checks last letter in the word that was given
+                char s = 's';
+            if (last == s) {
+                        System.out.print("How many " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase()  + " is needed in the recipe? ");
+                        amount = scan.nextLine();
+                        pw.write("* " + amount + " " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase() + "\n");
+                        num++;
+                    }
+                    else{
+                        System.out.print("How much " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase()  + " is needed in the recipe? ");
+                        amount = scan.nextLine();
+                        pw.write("* " + amount + " " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase()  + ""+ "\n");
+                        num++;
+                    }
+            }
+            
+            while (!text.equalsIgnoreCase("done")){
+                System.out.println("\nPlease enter the steps to make the recipe:");//print to user
+                System.out.println("When finished type 'done'");
+                System.out.print(count + ". "); 
+                text = scan.nextLine();// Take next input from user and save it in "text"
+
+                    if (text.equalsIgnoreCase("done")){
+                        text = "";
+                        break; // if the user input done break out of statment and do nothing to file.
+                    }
+
+                pw.write("* __Step "+ count + ".__ " + text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase()  + "\n\n");       
+                count++; 
+             
+                pw.write("---\n");
+                pw.flush();//flush the stream
+                pw.close();//close the file! 
+                
+            }
+            System.out.println("Your recipe has succesfully been added!\n"); // completed program
+            System.out.println("Do you want to add another Recipe? (y/n)");// go again?
 
             text = scan.next();// Take next input from user and save it in "text"
             scan.nextLine();// Eats the enter \n
         }
-        pw.write("---\n");
-        pw.flush();
-        pw.close();
-        System.out.println("Recipe has Succesfully been added!"); // completed program
+        
         }
     }
--- Recipe_List.md
+++ Recipe_List.md
@@ -1,4 +1,4 @@
-# Seasoned Chicken Breast
+# Seasoned Chicken Breast 
 |Amount/Ingredient|
 |:----------------:|
 | 2 Breasts Chicken|
@@ -31,5 +31,41 @@
 * __Step 2.__ add the vanilla, flour, salt, and sugar into the whipped eggs and milk
 * __Step 3.__ melt 1/4 - 1/2 cup of butter into a 9X13 pan
 * __Step 4.__ cook @ 350 for 20-30 mins
+
+---
+# Beef Hash
+
+|Amount/Ingredient|
+|:----------------:|
+| 2 tbls butter|
+| 3 cloves garlic|
+| 1 medium sized yellow or sweet onion|
+| 1 Lbs Ground Beef|
+| 5 stalks kale|
+| 2  bell peppers |
+| to taste lemon juice|
+| to taste hot sauce(your choice)|
+| 6 poached egg|
+* __Step 1.__ Heat butter in a medium/large saute pan
+
+* __Step 2.__ mince garlic and add to butter pan
+
+* __Step 3.__ while butter is sauteing, dice onion
+
+* __Step 4.__ add onion to garlic, once it becomes aromatic
+
+* __Step 5.__ While onion is sauteing, mince kale and dice bell peppers
+
+* __Step 6.__ As onions become glassy add kale, bell peppers, and ground beef
+
+* __Step 7.__ Cook until beef is cooked through
+
+* __Step 8.__ While beef is cooking, Make poached eggs
+
+* __Step 9.__ When beef is almost done add whatever spices are desired.
+
+* __Step 10.__ When beef is done, serve hot, with eggs on top of Beef Hash
+
+* __Step 11.__ Garnish with lemon juice and hot sauce.
 
 ---
